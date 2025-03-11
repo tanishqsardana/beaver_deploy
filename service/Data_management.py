7cd5879c86bfd5a3f3313a9ca07323e3bc1fc71c
Chris Ziyu Kong <zk2086@nyu.edu>
2025-03-11 11:33:04 -0400

    update credentials Chris0311

--- service/Data_management.py
+++ service/Data_management.py
@@ -20,17 +20,23 @@ import json
 from google.oauth2 import service_account
 
 
-SERVICE_ACCOUNT_FILE = "ee-beaver-lab-37f45e7fed2e.json"
-EE_SCOPES = ['https://www.googleapis.com/auth/earthengine']
-
-if not os.path.exists(SERVICE_ACCOUNT_FILE):
-    raise FileNotFoundError("Service account JSON file not found in /app/")
-
-with open(SERVICE_ACCOUNT_FILE, "r") as f:
-    credentials_info = json.load(f)
+credentials_info = {
+    "type": st.secrets["gcp_service_account"]["type"],
+    "project_id": st.secrets["gcp_service_account"]["project_id"],
+    "private_key_id": st.secrets["gcp_service_account"]["private_key_id"],
+    "private_key": st.secrets["gcp_service_account"]["private_key"],
+    "client_email": st.secrets["gcp_service_account"]["client_email"],
+    "client_id": st.secrets["gcp_service_account"]["client_id"],
+    "auth_uri": st.secrets["gcp_service_account"]["auth_uri"],
+    "token_uri": st.secrets["gcp_service_account"]["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["gcp_service_account"]["client_x509_cert_url"],
+    "universe_domain": st.secrets["gcp_service_account"]["universe_domain"]
+}
 
 credentials = service_account.Credentials.from_service_account_info(
-    credentials_info, scopes=EE_SCOPES
+    credentials_info,
+    scopes=["https://www.googleapis.com/auth/earthengine"]
 )
 
 ee.Initialize(credentials)
